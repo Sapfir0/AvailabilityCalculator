5749f08fbcebd76bdb245b23c2e3f15a40682adc
Sapfir0 <sapfir999999@yandex.ru>
2021-03-04 21:33:12 +0300

    Buffering

--- AvailabilityCalculator/__init__.py
+++ AvailabilityCalculator/__init__.py
@@ -1,15 +1,8 @@
-#-----------------------------------------------------------
-# Copyright (C) 2015 Martin Dobias
-#-----------------------------------------------------------
-# Licensed under the terms of GNU GPL 2
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#---------------------------------------------------------------------
-
+import os.path
 from PyQt5.QtWidgets import QAction, QMessageBox
+from qgis.core import QgsVectorLayer, QgsVectorFileWriter, QgsProject, QgsWkbTypes
+from qgis.utils import iface
+import processing
 
 def classFactory(iface):
     return MinimalPlugin(iface)
@@ -29,4 +22,21 @@ class MinimalPlugin:
         del self.action
 
     def run(self):
-        QMessageBox.information(None, 'AvailabilityCalculator', 'Availability calculator')
+        layerName = "Кадастр"
+        bufferDist = 500
+        layer = QgsProject.instance().mapLayersByName(layerName)[0]
+        if not layer.isValid():
+            QMessageBox.information(None, 'AvailabilityCalculator', f'Layer loading failed')
+        else:
+            print("Valid layer")
+            QgsProject.instance().addMapLayer(layer)
+
+        bufferLayer = processing.run("qgis:buffer", {'INPUT': layer, 'DISTANCE': bufferDist, 'OUTPUT': 'TEMPORARY_OUTPUT'})['OUTPUT']
+        QgsProject.instance().addMapLayer(bufferLayer)
+
+        processing.run('qgis:extractbylocation',  {'INPUT': layer, 'PREDICATE': [0], 'INTERSECT': bufferLayer, 'OUTPUT': 'TEMPORARY_OUTPUT'})
+
+
+
+
+
